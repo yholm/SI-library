e15ffb6a820f9f63279a32b0cbabcca2b0dd988d
Freyja Nyholm <theo.nyholm@gmail.com>
2025-02-10 11:04:36 +0100

    added literals for derived units

--- include/derived_si_units.h
+++ include/derived_si_units.h
@@ -17,8 +17,36 @@ using Newton    = SIUnit<Force>;
 using Pascal    = SIUnit<Pressure>;
 using Joule     = SIUnit<Energy>;
 using Watt      = SIUnit<Power>;
-using Couloumb  = SIUnit<ElectricCharge>;
+using Coulomb  = SIUnit<ElectricCharge>;
 using Volt      = SIUnit<ElectricPotential>;
 using Ohm       = SIUnit<ElectricResistance>;
+
+static Hertz operator"" _Hz(long double val) {
+    return Hertz(static_cast<double>(val));
+}
+
+static Newton operator"" _N(long double val) {
+    return Newton(static_cast<double>(val));
+}
+
+static Pascal operator"" _Pa(long double val) {
+    return Pascal(static_cast<double>(val));
+}
+
+static Joule operator"" _J(long double val) {
+    return Joule(static_cast<double>(val));
+}
+
+static Watt operator"" _W(long double val) {
+    return Watt(static_cast<double>(val));
+}
+
+static Coulomb operator"" _C(long double val) {
+    return Coulomb(static_cast<double>(val));
+}
+
+static Volt operator"" _V(long double val) {
+    return Volt(static_cast<double>(val));
+}
 
 #endif
